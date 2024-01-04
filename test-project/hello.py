1aa27c49e911dad9ebaf5e10faeb1bc6d4f8163f
sidsdro <70059519+sidsdro@users.noreply.github.com>
2024-01-04 21:53:34 +0100

    Update hello.py

--- test-project/hello.py
+++ test-project/hello.py
@@ -3,4 +3,7 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 
-print('Hello, remote world!')
+import tensorflow as tf
+
+# Load the .pb file
+converter = tf.lite.TFLiteConverter.from_saved_model("path/to/saved_model.pb")
